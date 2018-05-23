b727775c9d6010bc6579c0e2f8d10e9377afbf64
Zeus-PC <michael.201096@gmail.com>
2018-05-24 04:03:19 +0800

    Do reverse_rot.c, spavanac, and fix seven_wonders problem

--- seven_wonders.c
+++ seven_wonders.c
@@ -7,7 +7,7 @@ int power(int coef)
 }
 
 int main(int argc, char const *argv[]) {
-	int character, total = 0;
+	int character, total = 0, n  = 0;
 	int count_T, count_C, count_G;
 	count_C = count_G = count_T = 0;
 	while((character = getchar()) != '\n')
@@ -19,16 +19,19 @@ int main(int argc, char const *argv[]) {
 		else if(character == 'G')
 			++count_G;
 	}
-	if(count_C > 0 && count_G > 0 && count_T > 0)
+	total = power(count_C) + power(count_G) + power(count_T);
+
+	//find how to make n*7
+	/*if(count_C > 0 && count_G > 0 && count_T > 0)
 	{
-		total = power(count_C) + power(count_G) + power(count_T) + 7;
-	}
-	//else if ()
-	else if(count_C == 0 || count_G == 0 || count_T == 0)
+		if(count_C == count_G  && count_ )
+		n =
+	}*/
+	/*else if(count_C == 0 || count_G == 0 || count_T == 0)
 	{
 		total = power(count_C) + power(count_G) + power(count_T);
-	}
+	}*/
 
-	printf("%lf\n", sqrt(9));
+	//printf("%lf\n", sqrt(9));
 	return 0;
 }
