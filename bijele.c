6e766f6c499ba69a6604fdc8c459bc5976d0e9c9
zeus <michael.201096@gmail.com>
2018-05-24 18:50:22 +0800

    Bijele Complete

--- bijele.c
+++ bijele.c
@@ -4,6 +4,34 @@ int main(int argc, char const *argv[]) {
 	int king, queen, rooks, bishops, knights, pawns;
 
 	scanf("%d %d %d %d %d %d", &king, &queen, &rooks, &bishops, &knights, &pawns);
-	
+
+	if(king != 1 || king == 1)
+	{
+		king = 1 - king;
+	}
+	if(queen != 1 || queen == 1)
+	{
+		queen = 1 - queen;
+	}
+	if(rooks != 2 || rooks == 2)
+	{
+		rooks = 2 - rooks;
+	}
+
+	if(bishops != 2 || bishops == 2)
+	{
+		bishops = 2 - bishops;
+	}
+
+	if(knights != 2 || knights == 2)
+	{
+		knights = 2 - knights;
+	}
+	if(pawns != 8 || pawns == 8)
+	{
+		pawns = 8 - pawns;
+	}
+	printf("%d %d %d %d %d %d", king, queen, rooks, bishops, knights, pawns);
+
 	return 0;
 }
