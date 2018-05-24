86ba58e5569566716f7faa01381cd34520721f4f
Zeus-PC <michael.201096@gmail.com>
2018-05-24 13:17:13 +0800

    Update for bijele.c not complete

---
+++ bijele.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+	int king, queen, rooks, bishops, knights, pawns;
+
+	scanf("%d %d %d %d %d %d", &king, &queen, &rooks, &bishops, &knights, &pawns);
+	
+	return 0;
+}
