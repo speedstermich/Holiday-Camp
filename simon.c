42f4c8c8c0c63383dead65ee8c69d9e0bd2b36ec
Zeus-PC <michael.201096@gmail.com>
2018-05-27 12:22:02 +0800

    update Simons

--- simon.c
+++ simon.c
@@ -1,29 +1,29 @@
-#include <iostream>
-#include <cstring>
-#include <string>
-
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[]) {
-	int count = 0;
+	int count;
+	int i = 0, j;
+	int get;
 	char  input[100];
 	char compare[] = "Simon says";
 
-	std::cin>> count;
-
+	scanf("%d ", &count);
 	while (count--)
 	{
-
-		getline(input, sizeof(input));
-		for (int i = 0; i < 10; i++)
+		get = getchar();
+		input[i] = get;
+		i++;
+		for (i = 0; i < 10; i++)
 		{
-			for(int j = 0; j < strlen(compare); j++)
+			for(j = 0; j < strlen(compare); j++)
 			if(input[i] == compare[j])
 			{
 				for(i = 11; i < strlen(input); i++)
-					std::cout <<input[i] ;
+					printf("%c", input[i]);
 			}
 		}
+		printf("\n");
 		//std::cout << '\n';
 		//count--;
 	}
