676efc0f94501c0333a271a270a94c54a284f694
Michael Octavianus Korintus <michael.201096@gmail.com>
2018-07-30 23:51:13 +0700

    update

--- bigsum.cpp
+++ bigsum.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 
-#define MAX 1000
+#define MAX 10
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-	int max_num, array[MAX], total = 0;
-	int i;
+	int max_num;
+	long long int arr[MAX], total = 0;
 
 	std::cin >> max_num;
-	for (i = 0; i < max_num; i++) {
-		std::cin >> array[i];
-		total += array[i];
+	for (int i = 0; i < max_num; i++) {
+		std::cin >> arr[i];
+		total += arr[i];
 	}
-
 	std::cout << total << '\n';
 	return 0;
 }
