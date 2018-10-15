9dd83ca3e30633ec7db6ebe1245b991beab23bee
Michael Octavianus Korintus <michael.201096@gmail.com>
2018-10-15 14:20:30 +0800

    update

---
+++ picu_bank.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	int dollar, addition, month, addition2, target, testcase;
+	//Dollar = D, Addition = A, Addition2 = B, months = M, Target = X
+
+	std::cin >> testcase;
+	while (testcase--)
+	{
+		std::cin >> dollar >> addition >> month >> addition2 >> target;
+		/* algorithm for counting the months*/
+	}
+	std::cout << month << '\n';
+	return 0;
+}
